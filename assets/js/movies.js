f667671a9e29705a159f26f4fb51da0bb165a79a
BR3CKO <andybadin@outlook.com>
2023-06-18 23:49:26 -0300

    Creacion de CRUD backend y CRUD HTML

--- assets/js/movies.js
+++ assets/js/movies.js
@@ -1,38 +1,68 @@
-const movieList = document.getElementById("movieList");
-
-let movies = [];
-let genre = [];
+// Obtiene los elementos de "movieForm" dentro de la lista de peliculas
+const movieData = document.getElementById("movieForm");
+// Obtiene los elementos de "movieGenres" dentro de la lista de peliculas
+const genreList = document.getElementsById("movieGenres");
+const movies = [];
 
 // Función para cargar peliculas desde el HTML
-function newData() { // Obtener los elementos "movie" dentro de la lista de peliculas
-    const movieData = movieList.getElementsByClassName("movie");
-
-    // Recorrer los elementos y extraer los datos
+function newData() {
+    // Recorre los elementos y extrae los datos
     for (let i = 0; i < movieData.length; i++) {
-        const tmpMovie = movieData[i];
-        const id = parseInt(tmpMovie.getAttribute("data-id"));
-        const img = tmpMovie.getAttribute("data-img")
-        const name = tmpMovie.getAttribute("data-name");
-        const year = parseInt(tmpMovie.getAttribute("data-year"));
-        const director = tmpMovie.getAttribute("data-director");
+        // Creo un objeto con todos los datos obetnidos en cada iteracion del array
+        if (movieData.id === movies.id) {
+            let mensaje = "ya existe";
+        } else {
+            const tmpMovie = movieData[i];
+            const tmpId = tmpMovie.getAttribute("data-id");
+            const tmpImg = tmpMovie.getAttribute("data-img")
+            const tmpName = tmpMovie.getAttribute("data-name");
+            const tmpYear = tmpMovie.getAttribute("data-year");
+            // Crea un array de géneros para luego agregarlos al Array movie
+            const tmpGenres = [
+                tmpMovie.getAttribute("data-genre1"),
+                tmpMovie.getAttribute("data-genre2"),
+                tmpMovie.getAttribute("data-genre3")
+            ];
+            const tmpDirector = tmpMovie.getAttribute("data-director");
 
-        // Crear el objeto persona y agregarlo al arreglo
-        const movie = [id, name, year, genre, director];
-        return movie;
+            // Crea el objeto movie y carga los datos obtenidos
+            const movie = {
+                id: tmpId,
+                img: tmpImg,
+                name: tmpName,
+                year: tmpYear,
+                genres: tmpGenres,
+                director: tmpDirector
+            };
+            return movie;
+        }
     }
 }
 
 function newMovie() {
-
-    newData();
+    movies.push(newData());
+    // for (let i = 0; i < movies.length; i++) {
+    //     const tmpMovie = array[i];
+    //     if (newData()[0] != tmpMovie[0]) {           
+    //     }
+    // }
 }
 
-function readMovie(id) {
+function readMovie() {
+    const tmpId = document.getElementById("movieID").value;
     // Buscar la película por su id
-    const movie = movies.find(movie => movie.id === id);
-
-    // Retornar la película encontrada
-    return movie;
+    for (let i = 0; i < movies.length; i++) {
+        tmpMovie = movies[i];
+        if (tmpMovie.id === tmpId) {
+            document.getElementById("movieName").textContent = tmpMovie.id;
+            document.getElementById("movieName").textContent = tmpMovie.img;
+            document.getElementById("movieName").textContent = tmpMovie.name;
+            document.getElementById("movieYear").textContent = tmpMovie.year;
+            document.getElementById("movieYear").textContent = tmpMovie.genres;
+            document.getElementById("movieYear").textContent = tmpMovie.director;
+            return;
+        }
+    }
 }
 function updateMovie() {
     const movie = movies.find(movie => movie.id === id);
@@ -42,7 +72,7 @@ function updateMovie() {
         const tmpMovie = array[i];
         if (movie == tmpMovie[0]) {
             newData();
-            
+
         }
     }
 
