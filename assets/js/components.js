41c136fc51fcfec2ea55894d6cc935bb771af8be
toriacostabel <victoriacostabel182@gmail.com>
2023-07-04 00:14:30 -0300

    made index ui responsive

--- assets/js/components.js
+++
@@ -1,3 +0,0 @@
-//carousel
-
-//hamburguer menu
--- assets/js/main.js
+++ assets/js/main.js
@@ -16,14 +16,14 @@ function retornarCardHTML(movie) {
   return `<div class="movie">
     <div class="img"><img src='${movie.img}' class="poster"></div>
     <div class="movieInformation" id="movieInformation">
-      <div class="name"><p class="infoP">${movie.name}</p></div>
-      <div class="time"><p class="infoP">Duration: ${movie.time}</p></div>
-      <div class="year"><p class="infoP">${movie.year}</p></div>
-      <div class="cost"><p class="infoP">USD ${movie.cost}</p></div>
-      <div class="genre"><p class="infoP">Genre: ${movie.genre}</p></div>
-      <div class="rating"><p class="infoP">Rating: ${movie.rating}</p></div>
-      <div class="director"><p class="infoP">Director: ${movie.director}</p></div>
-      <div class="buy"><button class="comprar"id="${movie.id}">Comprar</button></div>
+      <div class="name"><p class="infoP movieName">${movie.name}</p></div>
+      <div class="time"><p class="infoP showOnHover">Duration: ${movie.time}</p></div>
+      <div class="year"><p class="infoP showOnHover">${movie.year}</p></div>
+      <div class="cost"><p class="infoP showOnHover">USD ${movie.cost}</p></div>
+      <div class="genre"><p class="infoP showOnHover">Genre: ${movie.genre}</p></div>
+      <div class="rating"><p class="infoP showOnHover">Rating: ${movie.rating}</p></div>
+      <div class="director"><p class="infoP showOnHover">Director: ${movie.director}</p></div>
+      <div class="buy"><button class="comprar showOnHover"id="${movie.id}">Comprar</button></div>
     </div>
   </div>`;
 }
