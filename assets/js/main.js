6cde967db21727ccf130932c59468fa469730be4
toriacostabel <victoriacostabel182@gmail.com>
2023-07-03 23:33:39 -0300

    styled movie info

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,15 +15,17 @@ function getMoviesFromLocalStorage() {
 function retornarCardHTML(movie) {
   return `<div class="movie">
     <div class="img"><img src='${movie.img}' class="poster"></div>
-    <div class="name"><p>${movie.name}</p></div>
-    <div class="time"><p>${movie.time}</p></div>
-    <div class="year"><p>${movie.year}</p></div>
-    <div class="cost"><p>${movie.cost}</p></div>
-    <div class="genre"><p>${movie.genre}</p></div>
-    <div class="rating"><p>${movie.rating}</p></div>
-    <div class="director"><p>${movie.director}</p></div>
-    <div class="buy"><button id="${movie.id}">Comprar</button></div>
-            </div>`;
+    <div class="movieInformation" id="movieInformation">
+      <div class="name"><p class="infoP">${movie.name}</p></div>
+      <div class="time"><p class="infoP">Duration: ${movie.time}</p></div>
+      <div class="year"><p class="infoP">${movie.year}</p></div>
+      <div class="cost"><p class="infoP">USD ${movie.cost}</p></div>
+      <div class="genre"><p class="infoP">Genre: ${movie.genre}</p></div>
+      <div class="rating"><p class="infoP">Rating: ${movie.rating}</p></div>
+      <div class="director"><p class="infoP">Director: ${movie.director}</p></div>
+      <div class="buy"><button class="comprar"id="${movie.id}">Comprar</button></div>
+    </div>
+  </div>`;
 }
 
 function loadMovies(array) {
